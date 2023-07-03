4eb2031424434a7e173412111b65e46bbffb5a26
Adam <paleadz_@hotmail.com>
2023-07-03 13:39:15 +0100

    todays cfinal change

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
-import Header from "./Components/Header";
 import Nav from "./Components/Nav";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
-import { Routes, Route, Link } from "react-router-dom";
+import Book from "./Components/book";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Nav></Nav>
-      <Main></Main>
-      <Footer></Footer>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/book" element={<Book />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
